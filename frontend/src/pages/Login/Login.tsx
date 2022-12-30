f600aa4ce71df0c2a8f88e68e51f77d93b8680ba
Willian Guedes <willian.wg70@gmail.com>
2022-12-30 19:21:57 +0000

    feat: add login page

---
+++ frontend/src/pages/Login/Login.tsx
@@ -0,0 +1,25 @@
+import './Login.css'
+
+export function Login(){
+    return(
+        <div className="main-login">
+            <div className="left-login">
+                <h1>Economize com a Energia Solar!</h1>
+            </div>
+            <div className="right-login">
+                <div className="card-login">
+                    <h1>LOGIN</h1>
+                    <div className="textfield">
+                        <label htmlFor="username">Username</label>
+                        <input name="username" type="text" placeholder='username'/>
+                    </div>
+                    <div className="textfield">
+                        <label htmlFor="password">password</label>
+                        <input name="password" type="password" placeholder='password'/>
+                    </div>
+                    <button className="btn-login">Login</button>
+                </div>
+            </div>
+        </div>
+    )
+}
