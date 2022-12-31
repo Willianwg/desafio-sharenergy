a986346c7909eb2bb506bfc99ecae2f064051e1f
Willian Guedes <willian.wg70@gmail.com>
2022-12-31 00:26:53 +0000

    feat: implement Random User Generator api

---
+++ frontend/src/components/User/User.tsx
@@ -0,0 +1,23 @@
+import "./User.css";
+
+type UserProps = {
+    name: string;
+    email: string;
+    username: string;
+    age: number;
+    picture: string;
+}
+
+export function User(props: UserProps){
+    return (
+        <div className="user-container">
+            <img src={ props.picture } />
+            <div className="content">
+                <p className="name">{ props.name }</p>
+                <p className="username">@{ props.username }</p>
+                <p className="email">{ props.email }</p>
+                <p className="age">age: { props.age }</p>
+            </div>
+        </div>
+    )
+}
--- frontend/src/pages/Main/Main.tsx
+++ frontend/src/pages/Main/Main.tsx
@@ -1,10 +1,71 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header/Header";
+import { SearchBar } from "../../components/SearchBar/SearchBar";
+import { User } from "../../components/User/User";
+
 import "./Main.css";
 
-export function Main(){
-    return(
+type UserProps = {
+    name: {
+        title: string;
+        first: string;
+        last: string;
+    };
+
+    email: string;
+
+    login: {
+        uuid: string;
+        username: string;
+        password: string;
+        salt: string;
+        md5: string;
+        sha1: string;
+        sha256: string
+    }
+    dob: {
+        date: Date;
+        age: number;
+    },
+
+    picture: {
+        thumbnail: string;
+        medium: string;
+        large: string;
+    };
+}
+
+export function Main() {
+    const [users, setUsers] = useState<UserProps[]>([]);
+
+    useEffect(() => {
+        async function loadUsers() {
+            const response = await (await fetch("https://randomuser.me/api/?inc=picture,name,email,login,dob,?page=3&results=15&seed=abc")).json();
+
+            setUsers(response.results);
+        }
+
+        loadUsers();
+    }, []);
+
+    return (
         <div className="main-page">
             <Header />
+            <SearchBar />
+            <div className="users-list">
+                {
+                    users.map((user, key) => {
+                        return <User
+                            key={key}
+                            name={user.name.first + " " + user.name.last}
+                            username={user.login.username}
+                            email={user.email}
+                            age={user.dob.age}
+                            picture={user.picture.large}
+                        />
+                    })
+                }
+            </div>
         </div>
     )
 }
