84ac961285bd5353e3a77ce177d0ba53aab97ea8
Willian Guedes <willian.wg70@gmail.com>
2022-12-31 21:59:28 +0000

    test: add verifications to  createClient and deleteClient tests

---
+++ backend/src/application/useCases/Client/deleteClient.spec.ts
@@ -0,0 +1,41 @@
+import { inMemoryClientRepository } from "../../../../test/repositories/inMemory/clientRepository";
+import { UserNotFound } from "../errors/userNotFound";
+import { CreateClient } from "./createClient";
+import { DeleteClient } from "./deleteClient";
+
+describe("Create Client", () => {
+    it("should be able to delete a Client", async () => {
+        const clientRepository = new inMemoryClientRepository();
+        const createClient = new CreateClient(clientRepository);
+        const deleteClient = new DeleteClient(clientRepository);
+
+        const { client } = await createClient.execute({
+            name: "example name",
+            email: "example@email.com",
+            document: "123456",
+            phone: "(11) 4002-8922",
+            address: "Av. do Contorno, 2905, Santa Efigênia, Belo Horizonte – MG"
+        })
+
+        await deleteClient.execute({ clientId: client.id });
+
+        expect(clientRepository.clients).toHaveLength(0);
+    })
+
+    it("should not be able to delete a Client", async () => {
+        const clientRepository = new inMemoryClientRepository();
+        const createClient = new CreateClient(clientRepository);
+        const deleteClient = new DeleteClient(clientRepository);
+
+        await createClient.execute({
+            name: "example name",
+            email: "example@email.com",
+            document: "123456",
+            phone: "(11) 4002-8922",
+            address: "Av. do Contorno, 2905, Santa Efigênia, Belo Horizonte – MG"
+        })
+
+        expect( ()=> deleteClient.execute({ clientId: "wrong-id" }) ).rejects.toThrow(UserNotFound);
+        expect(clientRepository.clients).toHaveLength(1);
+    })
+})
