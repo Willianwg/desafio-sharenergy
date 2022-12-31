616ee5d8585df3d0bc7ef6fac902b93458dd1e63
Willian Guedes <willian.wg70@gmail.com>
2022-12-31 00:44:56 +0000

    feat: improve Main page styles

--- frontend/src/components/SearchBar/SearchBar.tsx
+++ frontend/src/components/SearchBar/SearchBar.tsx
@@ -7,7 +7,7 @@ export function SearchBar(){
 
     return(
         <div className="searchbar">
-            <input onChange={ e => setSearch(e.target.value)} type="text" className="bar" />
+            <input onChange={ e => setSearch(e.target.value)} type="text" className="bar"  placeholder='Search by name, email or username'/>
             <button className="btn">
                 <img src={ loupe } alt="loupe" className="bar-image"/>
             </button>
