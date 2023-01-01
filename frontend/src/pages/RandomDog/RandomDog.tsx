1aed9c1debe6ad85009f77a54e95e5e43ce6e03b
Willian Guedes <willian.wg70@gmail.com>
2023-01-01 17:35:34 +0000

    fix: randomDog page scale and footer's padding-top'

--- frontend/src/pages/RandomDog/RandomDog.tsx
+++ frontend/src/pages/RandomDog/RandomDog.tsx
@@ -15,12 +15,14 @@ export function RandomDog() {
     function loadPage() {
         return (
             <>
-                <div className="dog-card">
-                    <div className="card-top">
-                        <h1>RandomDog</h1>
-                        <button className="refresh-btn" onClick={loadDogs}><img src={RefreshImage} /></button>
+                <div className="dog-container">
+                    <div className="dog-card">
+                        <div className="card-top">
+                            <h1>RandomDog</h1>
+                            <button className="refresh-btn" onClick={loadDogs}><img src={RefreshImage} /></button>
+                        </div>
+                        <img src={`https://random.dog/${dog}`} />
                     </div>
-                    <img src={`https://random.dog/${dog}`} />
                 </div>
                 <Footer />
             </>
