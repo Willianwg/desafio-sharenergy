4cc18adfc791c37fb32686c7e48938dec98a1d9a
Willian <willian.wg70@gmail.com>
2023-01-01 22:28:01 -0300

    feat: add request validators

--- backend/src/infra/http/controllers/user.controller.ts
+++ backend/src/infra/http/controllers/user.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Login } from 'src/application/useCases/Login/login';
 import { CreateUser } from 'src/application/useCases/User/createUser';
+import { CreateUserDTO } from '../dtos/User/createUserDTO';
+import { LoginDTO } from '../dtos/User/loginDTO';
 
 @Controller("user")
 export class UserController {
@@ -10,14 +12,16 @@ export class UserController {
     ) { }
 
     @Post()
-    async create(@Body() user: { username: string, password: string }) {
+    async create(@Body() user: CreateUserDTO) {
         await this.createUser.execute(user);
     }
 
-    @Get()
-    async loginUser(@Body() user: { username: string, password: string }) {
-        const { username, password } = user;
-        await this.login.execute({ username, password });
+    @Get("login")
+    async loginUser(@Body() loginData: LoginDTO) {
+        const { username, password } = loginData;
+        const { user } = await this.login.execute({ username, password });
+
+        return user;
     }
 
 }
