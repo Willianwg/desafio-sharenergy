d230e9391788b696967341bf407c03f28afa2b22
Willian <willian.wg70@gmail.com>
2023-01-03 17:41:27 -0300

    feat: add http client mapper

--- backend/src/infra/http/controllers/client.controller.ts
+++ backend/src/infra/http/controllers/client.controller.ts
@@ -5,8 +5,8 @@ import { GetAllClients } from 'src/application/useCases/Client/getAllClients';
 import { GetClientDetails } from 'src/application/useCases/Client/getClientDetails';
 import { UpdateClient } from 'src/application/useCases/Client/updateClient';
 import { CreateClientDTO } from '../dtos/Client/createClientDTO';
-import { DeleteClientDTO } from '../dtos/Client/deleteClientDTO';
 import { UpdateClientDTO } from '../dtos/Client/updateClientDTO';
+import { httpClientMapper } from '../mappers/httpClientMapper';
 
 @Controller("clients")
 export class ClientController {
@@ -24,7 +24,7 @@ export class ClientController {
             const { client } = await this.getClientDetails.execute({ clientId: id });
 
             return {
-                client
+                client: httpClientMapper(client),
             }
         } catch {
             throw new NotFoundException();
@@ -36,23 +36,23 @@ export class ClientController {
         const { clients } = await this.getAllClients.execute();
 
         return {
-            clients,
+            clients: clients.map(httpClientMapper),
         }
     }
 
     @Post("create")
-    async create(@Body() client: CreateClientDTO) {
+    async create(@Body() client: CreateClientDTO): Promise<void> {
         await this.createClient.execute(client);
     }
 
     @Put(":id/update")
-    async update(@Body() updatedClientData: UpdateClientDTO, @Param("id") clientId: string) {
+    async update(@Body() updatedClientData: UpdateClientDTO, @Param("id") clientId: string): Promise<void> {
         await this.updateClient.execute({ ...updatedClientData, clientId });
 
     }
 
     @Delete(":id/delete")
-    async delete(@Param("id") clientId: string) {
+    async delete(@Param("id") clientId: string): Promise<void> {
         try {
             await this.deleteClient.execute({ clientId });
         } catch {
