aaebd9e3bec6e8a85035e8fda122735cbd6b7553
Willian <willian.wg70@gmail.com>
2023-01-04 17:54:37 -0300

    feat: add a question before deleting a client

--- frontend/src/components/Client/ClientDetails.tsx
+++ frontend/src/components/Client/ClientDetails.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import "./Client.css";
 import { TextArea } from "./Textarea";
 
@@ -12,24 +13,53 @@ type ClientDetailsProps = {
 }
 
 export function ClientDetails(props: ClientDetailsProps) {
+    const [deleteOption, setDelete] = useState(false);
+
+    function openDeleteOption(e: React.MouseEvent<HTMLButtonElement>) {
+        e.preventDefault();
+
+        setDele
+    }
+    function deletee() {
+        return (
+            <div className="delete-client">
+                <p>Are you sure about this?</p>
+                <div className="modal-buttons delete">
+                    <button className="modal-btn">Yes</button>
+                    <button className="modal-btn delete-btn" onClick={()=> setDelete(false)}>Cancel</button>
+                </div>
+            </div>
+        )
+    }
     return (
         <div className="transparent">
             <div className="client-container">
-                <div className="client-data">
-                    <button onClick={props.closeModal} className="modal-close">X</button>
-                    <TextArea label="name" value={props.name} />
-                    <TextArea label="email" value={props.email} />
-                    <TextArea label="phone" value={props.phone} />
-                    <TextArea label="cpf" value={props.document} />
-                    <TextArea label="address" value={props.address} />
-                    <TextArea label="id" value={props.id} />
-                   
-                </div>
-                <div className="modal-buttons">
-                        <button className="modal-btn">Edit</button>
-                        <button className="modal-btn delete-btn">Delete</button>
-                    </div>
+                { !deleteOption && clientData(props, setDelete) }
+                { deleteOption && deletee() }
             </div>
         </div>
+    )
+}
+
+
+
+function clientData(props: ClientDetailsProps, callback: (state:boolean)=> void) {
+    return (
+        <>
+            <div className="client-data">
+                <button onClick={props.closeModal} className="modal-close">X</button>
+                <TextArea label="name" value={props.name} />
+                <TextArea label="email" value={props.email} />
+                <TextArea label="phone" value={props.phone} />
+                <TextArea label="cpf" value={props.document} />
+                <TextArea label="address" value={props.address} />
+                <TextArea label="id" value={props.id} />
+
+            </div>
+            <div className="modal-buttons">
+                <button className="modal-btn">Edit</button>
+                <button className="modal-btn delete-btn" onClick={() => callback(true)}>Delete</button>
+            </div>
+        </>
     )
 }
